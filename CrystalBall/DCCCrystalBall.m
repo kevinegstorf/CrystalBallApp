dc1b0521278ef601ad6f9fc98c9ac1f7d91d581c
kevinegstorf <kevinegstorf@kevins-MacBook-Pro.local>
2014-08-02 09:37:30 +0200

    updated class

--- CrystalBall/DCCCrystalBall.m
+++ CrystalBall/DCCCrystalBall.m
@@ -10,9 +10,36 @@
 
 @implementation DCCCrystalBall
 
+- (NSArray *)predictions{
+    if (_predictions == nil){
+        _predictions = [[NSArray alloc]initWithObjects:@"It is Certain",
+                        @"It is Decidedly so",
+                        @"All signs say YES",
+                        @"The stars are not aligned",
+                        @"My reply is no",
+                        @"It is doubtful",
+                        @"Better no tell you now",
+                        @"Concentrate and ask again",
+                        @"Unable to answer now", nil];
+        
+
+
+    }
+    return _predictions;
+}
+
 - (NSString*) randomPrediction; {
-    return nil;
+    int random =arc4random_uniform(self.predictions.count);
+    return [self.predictions objectAtIndex:random];
     
 }
 
 @end
+
+
+
+
+
+
+
+
